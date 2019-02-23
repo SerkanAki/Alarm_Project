74ee76078ed4eaf54c97a6967cd80bec7505a38d
Serkan AKI <serkanaki95@gmail.com>
2019-02-23 23:10:55 +0300

    Alarm Project

---
+++ alarm/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace alarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDakika = new System.Windows.Forms.Label();
+            this.lblSaat = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnAlarmKur = new System.Windows.Forms.Button();
+            this.cmbDakika = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbSaat = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lblDakika);
+            this.panel1.Controls.Add(this.lblSaat);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(257, 89);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(120, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = ":";
+            // 
+            // lblDakika
+            // 
+            this.lblDakika.AutoSize = true;
+            this.lblDakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDakika.Location = new System.Drawing.Point(129, 50);
+            this.lblDakika.Name = "lblDakika";
+            this.lblDakika.Size = new System.Drawing.Size(27, 18);
+            this.lblDakika.TabIndex = 3;
+            this.lblDakika.Text = "Dk";
+            // 
+            // lblSaat
+            // 
+            this.lblSaat.AutoSize = true;
+            this.lblSaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSaat.Location = new System.Drawing.Point(101, 50);
+            this.lblSaat.Name = "lblSaat";
+            this.lblSaat.Size = new System.Drawing.Size(26, 18);
+            this.lblSaat.TabIndex = 2;
+            this.lblSaat.Text = "Sa";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(61, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sistem Saatiniz";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.btnAlarmKur);
+            this.panel2.Controls.Add(this.cmbDakika);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.cmbSaat);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(2, 94);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(257, 190);
+            this.panel2.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(5, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Kişisel Mesaj";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(89, 139);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 38);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Tomato;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(136, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 25);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Alarmı Kapat";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnAlarmKur
+            // 
+            this.btnAlarmKur.BackColor = System.Drawing.Color.Tomato;
+            this.btnAlarmKur.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAlarmKur.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAlarmKur.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAlarmKur.Location = new System.Drawing.Point(26, 108);
+            this.btnAlarmKur.Name = "btnAlarmKur";
+            this.btnAlarmKur.Size = new System.Drawing.Size(94, 25);
+            this.btnAlarmKur.TabIndex = 4;
+            this.btnAlarmKur.Text = "Alarmı Kur";
+            this.btnAlarmKur.UseVisualStyleBackColor = false;
+            this.btnAlarmKur.Click += new System.EventHandler(this.btnAlarmKur_Click);
+            // 
+            // cmbDakika
+            // 
+            this.cmbDakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbDakika.FormattingEnabled = true;
+            this.cmbDakika.Location = new System.Drawing.Point(81, 63);
+            this.cmbDakika.Name = "cmbDakika";
+            this.cmbDakika.Size = new System.Drawing.Size(132, 24);
+            this.cmbDakika.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(24, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Dakika :";
+            // 
+            // cmbSaat
+            // 
+            this.cmbSaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbSaat.FormattingEnabled = true;
+            this.cmbSaat.Location = new System.Drawing.Point(81, 23);
+            this.cmbSaat.Name = "cmbSaat";
+            this.cmbSaat.Size = new System.Drawing.Size(132, 24);
+            this.cmbSaat.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(24, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Saat :";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(29, 291);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(220, 49);
+            this.axWindowsMediaPlayer1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(261, 283);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alarm V1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDakika;
+        private System.Windows.Forms.Label lblSaat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAlarmKur;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ComboBox cmbDakika;
+        private System.Windows.Forms.ComboBox cmbSaat;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ alarm/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace alarm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            suan();
+            for (int i = 0; i < 24; i++)
+            {
+                cmbSaat.Items.Add(i);
+            }
+            for (int j = 0; j < 60; j++)
+            {
+                cmbDakika.Items.Add(j);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblSaat.Text = DateTime.Now.Hour.ToString();
+            lblDakika.Text = DateTime.Now.Minute.ToString();
+            if (cmbSaat.Text == lblSaat.Text && cmbDakika.Text == lblDakika.Text)
+            {
+                timer1.Stop();
+                axWindowsMediaPlayer1.settings.volume = 100;
+                axWindowsMediaPlayer1.URL = Application.StartupPath + "/Alarm-Sesi.mp3";
+                MessageBox.Show(textBox1.Text);
+            }
+        }
+
+        private void btnAlarmKur_Click(object sender, EventArgs e)
+        {
+            suan();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            suan();
+            axWindowsMediaPlayer1.close();
+            foreach (ComboBox cmbx in panel2.Controls.OfType<ComboBox>())
+            {
+                cmbx.Text = "";
+            }
+        }
+        void suan()
+        {
+            timer1.Start();
+            lblSaat.Text = DateTime.Now.Hour.ToString();
+            lblDakika.Text = DateTime.Now.Minute.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
